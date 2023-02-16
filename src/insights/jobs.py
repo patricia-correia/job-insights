eb6111265985e15d2f32c66bfbf1ee2e34c39393
Patrícia Correia <pncorreia25@gmail.com>
2023-02-16 14:54:25 -0300

    começando o projeto installando as dependencia

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -39,7 +39,6 @@ def get_unique_job_types(path: str) -> List[str]:
 
 def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
     """Filters a list of jobs by job_type
-
     Parameters
     ----------
     jobs : list
