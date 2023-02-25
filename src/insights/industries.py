28e4362ebe46e246810b40f1ab7a92e3b103bb19
Patrícia Correia <pncorreia25@gmail.com>
2023-02-25 18:12:47 -0300

    leitura e filtro de industry

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -1,37 +1,19 @@
+from src.insights.jobs import read
 from typing import List, Dict
 
 
 def get_unique_industries(path: str) -> List[str]:
-    """Checks all different industries and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    raise NotImplementedError
+    csvfile = read(path)
+    industry = []
+    for jobs in csvfile: 
+        if jobs["industry"] not in industry and jobs["industry"] != '':
+            industry.append(jobs["industry"])
+    return industry
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
-    """Filters a list of jobs by industry
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
-    raise NotImplementedError
+    filter_industry_types = []
+    for job_industry in jobs:
+        if job_industry["industry"] == industry:
+            filter_industry_types.append(job_industry)
+    return filter_industry_types
