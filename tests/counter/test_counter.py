fc577215f9e6b8e87767f6bed90d121bc18481e0
Patricia Correia <pncorreia25@gmail.com>
2023-03-01 10:52:20 -0300

    teste conte quantas vezes aparece a palavra

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,10 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    path = 'data/jobs.csv'
+    count_python = count_ocurrences(path, 'python')
+    count_tasks = count_ocurrences(path, 'tasks')
+
+    assert count_python == 1639
+    assert count_tasks == 745
